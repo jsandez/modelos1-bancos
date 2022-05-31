481ca9bd9a086292a09132f8190b810dfdbba7ea
Jorge Sandez <a446044@santandertecnologia.com.ar>
2022-05-31 13:16:07 -0300

    mejoras en codigo

--- dineroSeguro.py
+++ dineroSeguro.py
@@ -23,53 +23,77 @@ def getData():
 	except:
 		print("falta cargar los archivos de entrada")
 
+def minimoConDemandasValidas(demandas, dimension, matrix, minX, capacidad):
+	demandasMinX = {index + 1: demandas[minX - 1] + demandas[index] for index, value in enumerate(range(dimension))}
+	demandasMinXFiltradas = dict(filter(lambda x: x[1] >= 0 and x[1] <= capacidad and x[0] != minX, demandasMinX.items()))
+	distanciasDemandasFiltradas = {index: matrix[minX -1][index - 1] for index, value in demandasMinXFiltradas.items()}
+	return min(distanciasDemandasFiltradas.items(), key = lambda x: x[1])
+
+def getSucursalesIniciales(demandas, dimension, capacidad, matrix, sucDemPositiva):
+	minX = list(sucDemPositiva)[0]
+	minimoPar = minimoConDemandasValidas(demandas, dimension, matrix, minX, capacidad)
+	minimo = minimoPar[1]
+	minY = minimoPar[0]
+
+	for i in sucDemPositiva:
+		minimoPar = minimoConDemandasValidas(demandas, dimension, matrix, i, capacidad)
+		
+		if (minimoPar[1] < minimo):
+			minimo = minimoPar[1]
+			minX = i
+			minY = minimoPar[0]
+	
+	return minimo, minX, minY
+
+
 def createOutput():
 	capacidad, dimension, demandas, matrix = getData()
 
 	mapaDemandas =  {index + 1: value for index, value in enumerate(demandas)}
 
-	primerasSucursales = dict(filter(lambda x: x[1] >= 0, mapaDemandas.items()))
+	sucDemPositiva = dict(filter(lambda x: x[1] >= 0, mapaDemandas.items()))
 
-	lists = []
+	secuencia = []
+
+	print("Buscando dos primeras sucursales")
+	minimo, minX, minY = getSucursalesIniciales(demandas, dimension, capacidad, matrix, sucDemPositiva.keys())
+
+	print("La minima distancia es {} y pasa en X: {} e Y: {}".format(minimo, minX, minY))
+	secuencia.append(minX)
+	secuencia.append(minY)
+	noVisitados = set(range(1,dimension + 1))
+	noVisitados.remove(minX)
+	noVisitados.remove(minY)
 
 	print("Arranca procesamiento")
 
-	for k in primerasSucursales.keys():
-		print("Creando iteracion para sucursal {} con demanda {}".format(k,demandas[k-1]))
-		secuencia = []
-		secuencia.append(k)
-		visitados = set()
-		visitados.add(k)
-		pendientes = set(range(1,dimension + 1)) - visitados
+	while (len(secuencia) < dimension):
+		print("Iteracion: {}".format(len(secuencia)))
 		acum = 0
-		acum += demandas[k - 1]
-		while (len(pendientes) > 0):
-			for i in pendientes:
-				if ((i not in secuencia)
-					and (acum + demandas[i -1] >= 0)
-					and (acum + demandas[i -1] <= capacidad)):
-					acum += demandas[i - 1]
-					secuencia.append(i)
-					visitados.add(i)
-			
-			pendientes = pendientes - visitados
+		for i in secuencia:
+		 	acum += demandas[i-1]
+		#print("arranca noVisitados")
+		#noVisitados = {i + 1 for i in range(dimension) if i + 1 not in secuencia}
+		print("arranca demandasTep")
+		demandasTemp = {value: acum + demandas[value - 1] for value in noVisitados}
+		print("arranca demandasTempFiltradas")
+		demandasTempFiltradas = dict(filter(lambda x: x[1] >= 0 and x[1] <= capacidad and x[0] in noVisitados, demandasTemp.items()))
+		print("arranca distanciasDemandasFiltradas")
+		distanciasDemandasFiltradas = {index: matrix[secuencia[-1] - 1][index - 1] for index, value in demandasTempFiltradas.items()}
+		print("arranca minimoPar")
+		minimoPar = min(distanciasDemandasFiltradas.items(), key = lambda x: x[1])
+		noVisitados.remove(minimoPar[0])
+		secuencia.append(minimoPar[0])
 
-		lists.append(list(secuencia))
+	minDistancia = 0
+	for i in range(len(secuencia) - 1):
+		minDistancia += matrix[secuencia[i] - 1][secuencia[i+1] - 1]
 
-	distancias=[]
-	for i in range(len(lists)):
-		acum = 0
-		for j in range(len(lists) - 1):
-			acum += matrix[lists[i][j] - 1][lists[i][j+1] - 1]
-		distancias.append(acum)
-
-	minDistancia = min(distancias)
-	nroIteracion = distancias.index(minDistancia)
-	print("La minima distancia es {} y paso en la iteracion numero {}".format(minDistancia, nroIteracion))
+	print("La minima distancia es {}".format(minDistancia))
 
 	fileOut = open("output.txt","w")
-	for i in range(len(lists[nroIteracion])):
-		fileOut.write("{} ".format(lists[nroIteracion][i]))
+	for i in range(len(secuencia)):
+		fileOut.write("{} ".format(secuencia[i]))
 	fileOut.close()
 
 createOutput()
